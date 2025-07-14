95d4c7beaccf8419a3d799552cedf6822d4a0e1b
Crist Yang <cr1st4ever@outlook.com>
2025-07-14 16:51:12 +0800

    day2 商铺redis缓存更新

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -9,10 +9,14 @@ import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 
+import java.util.concurrent.TimeUnit;
+
 import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TTL;
 
 /**
  * <p>
@@ -44,7 +48,22 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
         }
         // 4. 将数据库中的数据保存到redis中
         stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id, JSONUtil.toJsonStr(shop));
+        stringRedisTemplate.expire(CACHE_SHOP_KEY + id, CACHE_SHOP_TTL, TimeUnit.MINUTES);
         // 5. 返回
         return Result.ok(shop);
+    }
+
+    @Override
+    @Transactional
+    public Result update(Shop shop) {
+        // 1. 更新
+        updateById(shop);
+        Long id = shop.getId();
+        if (id == null) {
+            return Result.fail("店铺id不能为空");
+        }
+        // 2. 删除缓存
+        stringRedisTemplate.delete(CACHE_SHOP_KEY + shop.getId());
+        return Result.ok();
     }
 }
--- src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
@@ -6,10 +6,12 @@ import com.hmdp.dto.Result;
 import com.hmdp.entity.ShopType;
 import com.hmdp.mapper.ShopTypeMapper;
 import com.hmdp.service.IShopTypeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -24,6 +26,7 @@ import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TYPE;
  * @since 2021-12-22
  */
 @Service
+@Slf4j
 public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
 
     @Resource
@@ -33,7 +36,11 @@ public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> i
     public Result queryShopType() {
         Set<String> shopTypes = stringRedisTemplate.opsForZSet().range(CACHE_SHOP_TYPE , 0, -1);
         if (shopTypes != null && !shopTypes.isEmpty()) {
-            return Result.ok(shopTypes);
+            List<ShopType> shopTypesList = new ArrayList<>();
+            for (String shopType : shopTypes) {
+                shopTypesList.add(JSONUtil.toBean(shopType, ShopType.class));
+            }
+            return Result.ok(shopTypesList);
         }
 
         List<ShopType> shopTypeList = query().orderByAsc("sort").list();
@@ -43,6 +50,7 @@ public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> i
         for (ShopType shopType : shopTypeList) {
             stringRedisTemplate.opsForZSet().add(CACHE_SHOP_TYPE, JSONUtil.toJsonStr(shopType), shopType.getSort());
         }
+        log.debug("店铺分类查询：{}", shopTypeList);
         return Result.ok(shopTypeList);
     }
 }
